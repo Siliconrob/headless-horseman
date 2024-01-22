f08595d6d9d17e79d0aced380d3c08ecb339d259
Robin Michael <Siliconrob@siliconheaven.net>
2024-01-21 20:44:59 -0600

    Using docker images instead now

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-import subprocess
 from icecream import ic
 import uvicorn
 from app import app
 
 ic.configureOutput(prefix='|> ')
 
-
-def setup_playwright():
-    result = ic(subprocess.run("playwright --version", shell=True, capture_output=True))
-    if result.returncode != 0:
-        ic(subprocess.run("playwright install", shell=True, capture_output=True))
-
-
 if __name__ == "__main__":
-    setup_playwright()
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    ic(uvicorn.run(app, host="0.0.0.0", port=8080))
