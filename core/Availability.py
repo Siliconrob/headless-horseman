4914e92856a430c44aff12da85e063afec0468d1
Robin Michael <Siliconrob@siliconheaven.net>
2024-07-15 10:24:31 -0500

    match js code in a looser manner

--- core/Availability.py
+++ core/Availability.py
@@ -13,13 +13,15 @@ def extract_availability(page_contents: str) -> list:
     if page_contents is None or len(page_contents) == 0:
         return parsed_results
 
+    match_date_pattern = r'(?:const|var|let)\s*bookedDates\s*=\s*([^.map;]*)'
+
     soup = BeautifulSoup(page_contents, "html.parser")
     scripts = soup.find_all("script", {"type": ["text/javascript"]})
     for script in scripts:
         replacements = {'\r': '', '\n': ''}
         pattern = '|'.join(replacements.keys())
         script_text = re.sub(pattern, lambda z: replacements[z.group(0)], script.get_text())
-        match = re.search('const bookedDates = ([^.map;]*)', script_text, re.IGNORECASE | re.MULTILINE)
+        match = re.search(match_date_pattern, script_text, re.IGNORECASE | re.MULTILINE)
         if match is None:
             continue
         availability_params = ic(match.groups()[0].replace('const bookedDates = ', ''))
@@ -43,14 +45,17 @@ def compute_availability(availability_params):
 
 def get_unavailable_dates(availability_params):
     unavailable_dates = []
-    for date_segment in eval(availability_params):
-        if isinstance(date_segment, collections.abc.Iterable):
-            end_date = ic(pendulum.parse(date_segment.pop()))
-            start_date = ic(pendulum.parse(date_segment.pop()))
-            date_interval = pendulum.interval(start_date, end_date)
-            for date in date_interval:
-                unavailable_dates.append(date.date())
-            continue
-        unavailable_dates.append(pendulum.parse(date_segment).date())
+    try:
+        for date_segment in eval(availability_params):
+            if isinstance(date_segment, collections.abc.Iterable):
+                end_date = ic(pendulum.parse(date_segment.pop()))
+                start_date = ic(pendulum.parse(date_segment.pop()))
+                date_interval = pendulum.interval(start_date, end_date)
+                for date in date_interval:
+                    unavailable_dates.append(date.date())
+                continue
+            unavailable_dates.append(pendulum.parse(date_segment).date())
+    except Exception as error:
+        ic(error)
     results = list(dict.fromkeys(unavailable_dates))
     return results
